da59161c08a7ae554fa60d76d23442452b939f02
ahmadanwar93 <ahmadfikri.anwarfadzil@gmail.com>
2024-09-25 17:45:17 +0800

    fix: icon-size

--- apps/web/src/icons/column-collapse.tsx
+++ apps/web/src/icons/column-collapse.tsx
@@ -1,9 +1,9 @@
 export default function ColumnCollapse({ ...props }) {
   return (
     <svg
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
@@ -11,35 +11,35 @@ export default function ColumnCollapse({ ...props }) {
       <g id="Icon/Column-collapse">
         <path
           id="Vector 146"
-          d="M12.8 4.80005L9.59998 8.00005L12.8 11.2"
-          stroke="#2563EB"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+          d="M16 6L12 10L16 14"
+          stroke="#18181B"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         />
         <path
           id="Vector 147"
-          d="M3.20001 11.2L6.40001 8.00005L3.20001 4.80005"
-          stroke="#2563EB"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+          d="M4 14L8 10L4 6"
+          stroke="#18181B"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         />
         <path
           id="Vector 160"
-          d="M9.59998 8L14.4 8"
-          stroke="#2563EB"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+          d="M12 10L18 10"
+          stroke="#18181B"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         />
         <path
           id="Vector 161"
-          d="M1.59998 8H6.39998"
-          stroke="#2563EB"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+          d="M2 10H8"
+          stroke="#18181B"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         />
       </g>
     </svg>
--- apps/web/src/icons/column-expand.tsx
+++ apps/web/src/icons/column-expand.tsx
@@ -1,9 +1,9 @@
 export default function ColumnExpand({ ...props }) {
   return (
     <svg
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
@@ -11,35 +11,35 @@ export default function ColumnExpand({ ...props }) {
       <g id="Icon/Column-expand">
         <path
           id="Vector 146"
-          d="M4.80001 4.79999L1.60001 7.99999L4.80001 11.2"
-          stroke="#2563EB"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+          d="M6 6L2 10L6 14"
+          stroke="#18181B"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         />
         <path
           id="Vector 147"
-          d="M11.2 11.2L14.4 7.99999L11.2 4.79999"
-          stroke="#2563EB"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+          d="M14 14L18 10L14 6"
+          stroke="#18181B"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         />
         <path
           id="Vector 160"
-          d="M1.60001 8L6.40001 8"
-          stroke="#2563EB"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+          d="M2 10L8 10"
+          stroke="#18181B"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         />
         <path
           id="Vector 161"
-          d="M9.60001 8H14.4"
-          stroke="#2563EB"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+          d="M12 10H18"
+          stroke="#18181B"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         />
       </g>
     </svg>
