f61f35b74e74e2c999d9fdcbf765c2fd2b1d3522
Len E-Herng <49067812+leneherng@users.noreply.github.com>
2024-10-28 00:11:44 +0800

    chore: remove unused

--- apps/web/src/icons/chevron-down.tsx
+++ apps/web/src/icons/chevron-down.tsx
@@ -24,22 +24,5 @@ export default function ChevronDown({ filled = false, ...props }) {
         />
       )}
     </svg>
-
-    // <svg
-    //   width={20}
-    //   height={20}
-    //   viewBox="0 0 20 20"
-    //   fill="none"
-    //   xmlns="http://www.w3.org/2000/svg"
-    //   {...props}
-    // >
-    //   <g id="Icon/fill/chevron-down">
-    //     <path
-    //       id="Vector"
-    //       d="M6.25251 7C5.13887 7 4.58117 8.34643 5.36862 9.13388L9.16152 12.9268C9.64967 13.4149 10.4411 13.4149 10.9293 12.9268L14.7222 9.13388C15.5096 8.34643 14.9519 7 13.8383 7H6.25251Z"
-    //       fill="currentColor"
-    //     />
-    //   </g>
-    // </svg>
   );
 }
