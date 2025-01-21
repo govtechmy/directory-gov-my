715a69324d82e643b9fa42354af28ee5b3991b27
Len E-Herng <49067812+leneherng@users.noreply.github.com>
2025-01-21 09:33:44 +0800

    chore: extend tailwind merge with font size class groups

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,6 +1,26 @@
 import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
 import { OfficeDirectory } from "./types/kakitangan";
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      "font-size": [
+        "text-heading-xl",
+        "text-heading-lg",
+        "text-heading-md",
+        "text-heading-sm",
+        "text-heading-xs",
+        "text-heading-2xs",
+        "text-body-xl",
+        "text-body-lg",
+        "text-body-md",
+        "text-body-sm",
+        "text-body-xs",
+      ],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
